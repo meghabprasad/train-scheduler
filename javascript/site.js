cba69a478929a64f63156b909b594150be548041
Megha Prasad Bindiganavale <Megha@Meghas-MacBook-Pro.local>
2019-06-11 23:09:48 -0700

    add minute.js calculations to retrieve minutes till next train

--- javascript/site.js
+++ javascript/site.js
@@ -16,6 +16,8 @@ var firebaseConfig = {
   var destination = "";
   var time = "";
   var frequency = "";
+  var nextTrain = "";
+  var tMinutesTillTrain = "";
   
 
 $("#add-train-btn").on("click", function(event){
@@ -25,13 +27,45 @@ $("#add-train-btn").on("click", function(event){
     time = $("#time-input").val().trim();
     frequency = $("#frequency-input").val().trim();
 
+    // Assumptions
+    var tFrequency = frequency;
+
+    // Time is 3:30 AM
+    var firstTime = time;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
 
     database.ref().push({
         trainNameF: trainName,
         destinationF: destination,
         timeF: time,
-        frequencyF: frequency
-    })
+        frequencyF: frequency,
+        nextTrainF: nextTrain,
+        tMinutesTillTrainF: tMinutesTillTrain
+    });
 })
 
 database.ref().on("child_added", function(snapshot){
@@ -41,22 +75,28 @@ database.ref().on("child_added", function(snapshot){
     console.log(snapshot.val().destinationF);
     console.log(snapshot.val().timeF);
     console.log(snapshot.val().frequencyF);
+    console.log(snapshot.val().nextTrainF);
+    console.log(snapshot.val().tMinutesTillTrainF);
+
 
     var row = $("<tr>");
     var nameData = $("<td>");
     var destinationData = $("<td>");
-    var timeData = $("<td>");
+    var nextTrainData = $("<td>");
     var frequencyData = $("<td>");
+    var tMinutesTillTrainData = $("<td>");
 
     nameData.text(snapshot.val().trainNameF);
     destinationData.text(snapshot.val().destinationF);
-    timeData.text(snapshot.val().timeF);
     frequencyData.text(snapshot.val().frequencyF);
+    nextTrainData.text(snapshot.val().nextTrainF);
+    tMinutesTillTrainData.text(snapshot.val().tMinutesTillTrainF);
 
     row.append(nameData);
     row.append(destinationData);
-    row.append(timeData);
     row.append(frequencyData);
+    row.append(nextTrainData);
+    row.append(tMinutesTillTrainData);
     $("#table-head").after(row);
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
